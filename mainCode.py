8578ddf36584a84a04ef7ecd98e1430d252d53d0
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-11-13 15:15:58 +0000

    np poly fit working

--- mainCode.py
+++ mainCode.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+
 csv_in_use = "children-born-per-woman.csv"
 
 
@@ -19,20 +22,39 @@ fertility_df = read_fetrtility()
 def get_spanish_fertility():
 
     spain_fertility_df= fertility_df[fertility_df['Entity'] == 'Spain'].copy()
- 
+    # Make sure Year is datetime first
+    spain_fertility_df["Year"] = pd.to_datetime(spain_fertility_df["Year"], errors="coerce")
+
+    # Extract the year as an integer
+    spain_fertility_df["Year"] = spain_fertility_df["Year"].dt.year
+
     return spain_fertility_df
 
 spain_fertility_df = get_spanish_fertility()
 
-print(spain_fertility_df)
+# print(spain_fertility_df)
 
-def plot_fertility_women_graph():
-#plotting both OECD women and UK same graph
-    plot_fertility_graph= spain_fertility_df.plot(kind='line',y="Fertility rate (period), historical", label='Fertility rate (period), historical',  linestyle='-')
-    plot_fertility_graph.set_title("Fertility rate in Spain  (1970-2018)")
-    plt.show()
+# def plot_fertility_women_graph():
+# #plotting both OECD women and UK same graph
+#     plot_fertility_graph= spain_fertility_df.plot(kind='line',y="Fertility rate (period), historical", label='Fertility rate (period), historical',  linestyle='-')
+#     plot_fertility_graph.set_title("Fertility rate in Spain  (1970-2018)")
+#     plt.show()
 
 # fertility_graph = plot_fertility_women_graph()
 
+
+
 x = spain_fertility_df["Year"]
 y = spain_fertility_df["Fertility rate (period), historical"]
+
+# Perform linear fit
+coefficients = np.polyfit(x, y, 6)
+print("Linear Fit Coefficients:", coefficients)
+
+# Create polynomial function
+p = np.poly1d(coefficients)
+
+plt.scatter(x, y, label='Data Points')
+plt.plot(x, p(x), label='Linear Fit', color='red')
+plt.legend()
+plt.show()
